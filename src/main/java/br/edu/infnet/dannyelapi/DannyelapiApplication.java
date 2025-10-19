ecb50d3a51fb38a32bc05630a05d9d0d52b49903
Dannyel Kayke <dannyel_kayke@hotmail.com>
2025-10-19 12:45:21 -0300

    initial project

---
+++ src/main/java/br/edu/infnet/dannyelapi/DannyelapiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.dannyelapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DannyelapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DannyelapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/infnet/dannyelapi/DannyelapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.dannyelapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DannyelapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
